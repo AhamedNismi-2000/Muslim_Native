f56b553bbf0d9f322199ec6964a7160aa353bf46
Ahamed Nismi <mnanismi2000@gmail.com>
2026-06-22 20:24:30 +0530

    calculationMethods.ts

---
+++ prayer_app_backend/src/constants/calculationMethods.ts
@@ -0,0 +1,16 @@
+export const CALCULATION_METHODS = [
+  "MuslimWorldLeague",
+  "Egyptian",
+  "Karachi",
+  "UmmAlQura",
+  "Dubai",
+  "MoonsightingCommittee",
+  "NorthAmerica",
+  "Kuwait",
+  "Qatar",
+  "Singapore",
+  "Turkey",
+  "Tehran",
+];
+
+export const MADHABS = ["Shafi", "Hanafi"];
